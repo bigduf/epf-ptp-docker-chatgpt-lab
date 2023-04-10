dbb6a25c5c819ec3f9a15fd1d3b9b8a848189c58
Romain Dufour <73946865+bigduf@users.noreply.github.com>
2023-04-10 12:39:48 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import os
 import openai
 
@@ -22,4 +22,27 @@ def chatgpt():
     )
     return completion['choices'][0]['message']['content']
 
+@app.route('/generate_code', methods=['POST'])
+def generate_code():
+    # Get the JSON data from the request
+    data = request.args
+    language = data.get('language')
+    content = data.get('content')
 
+    # Call OpenAI API to generate code
+    completion = openai.ChatCompletion.create(
+        engine="davinci-codex",
+        prompt=f"Hello Chat, please generate {language} code: {content}",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    generated_code = completion['choices'][0]['text']
+
+    # Return generated code as JSON response
+    response = {
+        'generated_code': generated_code
+    }  
+
+    return jsonify(response)
